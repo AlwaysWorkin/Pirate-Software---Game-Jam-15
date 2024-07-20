63c536c59d2cee808351d404b1dc58c2f6b1d8d7
Alexander Kraemling <alexander.kraemling@gmail.com>
2024-07-20 23:09:48 +0200

    Super simple start up screen

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,13 +1,23 @@
 extends Camera3D
 
-var rotation_speed := 0.01
+var rotation_speed := 0.5
 var min_angle := -45.0
 var max_angle := 45.0
 var current_yaw := 0.0
 
-func _input(event):
-	if event is InputEventMouseMotion:
-		var delta = event.relative
-		current_yaw -= delta.x * rotation_speed
-		current_yaw = clamp(current_yaw, min_angle * PI / 180, max_angle * PI / 180)
-		rotation_degrees.y = current_yaw * 180 / PI
+func _physics_process(delta):
+    var direction = Input.get_axis("pan_left", "pan_right")
+    if direction != 0:
+        current_yaw -= direction * rotation_speed * delta
+    
+    current_yaw = clamp(current_yaw, min_angle * PI / 180, max_angle * PI / 180)
+    rotation_degrees.y = current_yaw * 180 / PI
+
+        
+# Looking left/ right with mouse
+#func _input(event):
+    #if event is InputEventMouseMotion:
+        #var delta = event.relative
+        #current_yaw -= delta.x * rotation_speed
+        #current_yaw = clamp(current_yaw, min_angle * PI / 180, max_angle * PI / 180)
+        #rotation_degrees.y = current_yaw * 180 / PI
